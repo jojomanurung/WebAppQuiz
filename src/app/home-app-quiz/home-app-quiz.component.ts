96aa9229fb96d0dee46f2931a5de4c546693bd50
Joshua Melvin <joshuamlvn@gmail.com>
2021-06-19 22:01:39 +0700

    Remove back slash from routerLink button and add title object

--- src/app/home-app-quiz/home-app-quiz.component.ts
+++ src/app/home-app-quiz/home-app-quiz.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-app-quiz',
   templateUrl: './home-app-quiz.component.html',
-  styleUrls: ['./home-app-quiz.component.css']
+  styleUrls: ['./home-app-quiz.component.css'],
 })
 export class HomeAppQuizComponent implements OnInit {
+  title = 'Quick Quiz';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
