8eaa40015c8ea2af440179230aa48a3f15c2e24c
Joshua Melvin <joshuamlvn@gmail.com>
2021-06-27 01:11:59 +0700

    Add getQuestion() functionality

--- src/app/game-app-quiz/game-app-quiz.component.ts
+++ src/app/game-app-quiz/game-app-quiz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OpenTriviaDbService } from '../service/open-trivia-db.service';
 
 @Component({
   selector: 'app-game-app-quiz',
@@ -8,11 +9,35 @@ import { Component, OnInit } from '@angular/core';
 export class GameAppQuizComponent implements OnInit {
   show = false;
   game = true;
-  value = 50;
-  constructor() {
+
+  tempData: any = [];
+  allQuiz: any = [];
+
+  constructor(private apiDB: OpenTriviaDbService) {
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getQuestion();
+  }
+
+  getQuestion() {
+    this.apiDB.getQuiz().subscribe((resp: any) => {
+      this.tempData.push(resp['results']);
+      console.log(this.tempData);
+      if (this.tempData && this.tempData[0] && this.tempData[0].length) {
+        this.tempData.map((res: any) => {
+          res.forEach((element: any) => {
+            const items = {
+              question: element.question,
+              choices: element.incorrect_answers.concat(element.correct_answer),
+              correct_answer: element.correct_answer
+            };
+            this.allQuiz.push(items);
+          });
+        });
+      };
+      console.log(this.allQuiz);
+    });
   }
 
 }
