e148baa76ecfb2d2fea2f298bccc449df42d2c97
Joshua Melvin <joshuamlvn@gmail.com>
2021-07-03 00:03:38 +0700

    import reactive form module and new angular material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeAppQuizComponent } from './home-app-quiz/home-app-quiz.component';
 import { GameAppQuizComponent } from './game-app-quiz/game-app-quiz.component';
@@ -17,6 +18,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -37,6 +41,10 @@ import { MatCardModule } from '@angular/material/card';
     MatProgressSpinnerModule,
     MatProgressBarModule,
     MatCardModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatTooltipModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
