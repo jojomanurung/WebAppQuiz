ba99e436841cb3e6990de41feea291718f78e39c
Joshua Melvin <joshuamlvn@gmail.com>
2021-07-03 00:07:29 +0700

    add select field for category and difficulty

--- src/app/game-app-quiz/game-app-quiz.component.ts
+++ src/app/game-app-quiz/game-app-quiz.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Renderer2 } from '@angular/core';
 import { OpenTriviaDbService } from '../service/open-trivia-db.service';
 import { Router } from '@angular/router';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-game-app-quiz',
@@ -8,6 +9,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./game-app-quiz.component.css']
 })
 export class GameAppQuizComponent implements OnInit {
+
+  categories = new FormControl('', Validators.required);
+  difficulty = new FormControl('', Validators.required);
+
+  difficultyData = [
+    {name: 'Easy', type: 'easy'},
+    {name: 'Medium', type: 'medium'},
+    {name: 'Hard', type: 'hard'},
+  ];
+  categoriesData: any;
+
+  selectedCategory: any;
+  selectedDifficulty: any;
+
+  isOptionSelected = false;
 
   constructor(
     private apiDB: OpenTriviaDbService,
@@ -17,8 +33,33 @@ export class GameAppQuizComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.getCategories();
   }
 
+  getCategories() {
+    this.apiDB.getCategories().subscribe((resp: any) => {
+      console.log(resp);
+      this.categoriesData = resp.trivia_categories.map((res: any) => {
+        let id = res.id;
+        let name = res.name;
+        // console.log(id, name);
+        return {id, name}
+      });
+      console.log(this.categoriesData);
+    });
+  }
 
+  playGame() {
+    if (this.categories.value !== '' && this.difficulty.value !== '') {
+      this.isOptionSelected = true;
+    }
+  }
 
+  selectCategory() {
+    this.selectedCategory = this.categories.value;
+  }
+
+  selectDifficulty() {
+    this.selectedDifficulty = this.difficulty.value;
+  }
 }
